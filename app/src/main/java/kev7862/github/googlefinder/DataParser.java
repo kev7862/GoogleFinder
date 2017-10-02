305d7da49c188c1411be14336fdf8316edf2f030
kev7862 <kevmatite7862@gmail.com>
2017-10-02 18:00:19 +0300

    Declared my Variable String classes

--- app/src/main/java/kev7862/github/googlefinder/DataParser.java
+++ app/src/main/java/kev7862/github/googlefinder/DataParser.java
@@ -2,12 +2,19 @@ package kev7862.github.googlefinder;
 
 import android.os.AsyncTask;
 
+import com.google.android.gms.maps.GoogleMap;
+
 /**
  * Created by matite on 10/2/17.
  */
 
 //helps our UI run Swiftly
 public class DataParser extends AsyncTask<Object, String, String>{
+
+    String googlePlacesData;
+    GoogleMap mMap;
+    String Url;
+
     @Override
     protected String doInBackground(Object... objects) {
         return null;
