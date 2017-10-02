e4bfb00f046e85db1e6b53d122572aa9ea95ee2c
kev7862 <kevmatite7862@gmail.com>
2017-10-02 17:53:41 +0300

    created an AsyncTask method to help our UI run swiftly

---
+++ app/src/main/java/kev7862/github/googlefinder/DataParser.java
@@ -0,0 +1,20 @@
+package kev7862.github.googlefinder;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by matite on 10/2/17.
+ */
+
+//helps our UI run Swiftly
+public class DataParser extends AsyncTask<Object, String, String>{
+    @Override
+    protected String doInBackground(Object... objects) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
---
+++ app/src/main/java/kev7862/github/googlefinder/DownloadUrl.java
@@ -0,0 +1,8 @@
+package kev7862.github.googlefinder;
+
+/**
+ * Created by matite on 10/2/17.
+ */
+
+public class DownloadUrl {
+}
