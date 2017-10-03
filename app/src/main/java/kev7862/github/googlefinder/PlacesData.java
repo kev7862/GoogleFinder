6e3cd81d1586791456d915d7ef61b6e7d4ce23ae
kev7862 <kevmatite7862@gmail.com>
2017-10-03 12:38:31 +0300

    This method is going to show all the places in the List. Create Marker Options and then move the Camera to that Marker

--- app/src/main/java/kev7862/github/googlefinder/PlacesData.java
+++ app/src/main/java/kev7862/github/googlefinder/PlacesData.java
@@ -2,9 +2,16 @@ package kev7862.github.googlefinder;
 
 import android.os.AsyncTask;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by matite on 10/2/17.
@@ -33,5 +40,27 @@ public class PlacesData extends AsyncTask<Object, String, String> {
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+    }
+    //This method is going to show all the places in the List
+    private void showNearbyPlaces(List<HashMap<String, String>> nearbyPlaceList) {
+        for (int i = 0;i<nearbyPlaceList.size() ;i++) {
+            MarkerOptions markerOptions = new MarkerOptions();
+            HashMap<String, String> googlePlace = nearbyPlaceList.get(i);
+
+            String placeName = googlePlace.get("place_name");
+            String vacinity = googlePlace.get("vacinity");
+            double lat = Double.parseDouble( googlePlace.get("lat") );
+            double lng = Double.parseDouble( googlePlace.get("lng") );
+
+            LatLng latLng = new LatLng(lat, lng);
+            markerOptions.position(latLng);
+            markerOptions.title(placeName +" : "+ vacinity);
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
+            mMap.addMarker(markerOptions);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+
+        }
     }
 }
