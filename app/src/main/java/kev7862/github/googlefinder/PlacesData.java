54ba2295d073e73a20e86d015b21f742e253af0f
kev7862 <kevmatite7862@gmail.com>
2017-10-03 09:30:16 +0300

    Declared my member variables, passed in parameters, and throw an Exception incase we run through an error

--- app/src/main/java/kev7862/github/googlefinder/PlacesData.java
+++ app/src/main/java/kev7862/github/googlefinder/PlacesData.java
@@ -1,8 +1,37 @@
 package kev7862.github.googlefinder;
 
+import android.os.AsyncTask;
+
+import com.google.android.gms.maps.GoogleMap;
+
+import java.io.IOException;
+
 /**
  * Created by matite on 10/2/17.
  */
 
-public class PlacesData {
+public class PlacesData extends AsyncTask<Object, String, String> {
+
+    String googlePlacesData;
+    GoogleMap mMap;
+    String Url;
+
+    @Override
+    protected String doInBackground(Object... objects) {
+        mMap = (GoogleMap) objects[0];
+        Url = (String) objects[1];
+        DownloadUrl downloadUrl = new DownloadUrl();
+        try {
+            googlePlacesData = downloadUrl.readUrl(Url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
 }
