f8aa59493d6ec456e880b2fcfe5e30e8845ce5a6
kev7862 <kevmatite7862@gmail.com>
2017-10-05 12:34:20 +0300

    Checking location permission if its GRANTED or NOT

--- app/src/main/java/kev7862/github/googlefinder/MapsActivity.java
+++ app/src/main/java/kev7862/github/googlefinder/MapsActivity.java
@@ -43,6 +43,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LocationRequest locationRequest;
     private Location lastLocation;
     private Marker currentLocationMarker;
+    public static final int REQUEST_LOCATION_CODE = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -160,13 +161,29 @@ lastLocation = location;
         locationRequest.setFastestInterval(1000);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == getPackageManager().PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
             LocationServices.FusedLocationApi.requestLocationUpdates(client, locationRequest, this);
 
         }
 
     }
+// Checking Location Permission if its Granted or Not
+    public boolean checkLocationPermission(){
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION }, REQUEST_LOCATION_CODE);
+}
+        }
+
+        else{
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION }, REQUEST_LOCATION_CODE);
+        }
+        return false;
+    }
+
 
     @Override
     public void onConnectionSuspended(int i) {
